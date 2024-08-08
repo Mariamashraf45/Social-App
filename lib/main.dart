fbf4f87fe715e8b18a296530df9250233c38a1f5
mema <mariamsadek54@gmail.com>
2024-08-08 05:10:25 +0300

    update 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-
 import 'package:fire/global/app_them.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
 
 
+void main() {
   runApp(MyApp());
 }
 
@@ -13,9 +10,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Login Screen',
+      title: 'Social App',
       theme: appTheme,
-      home:Text('Flutter New Feature') ,
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to your social app!',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
